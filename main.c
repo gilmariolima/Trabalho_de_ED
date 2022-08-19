7e05b21bfecbe770a96838d1e5aa659ce6edfb16
Gilmario Lima <103860007+gilmariolima@users.noreply.github.com>
2022-08-19 16:39:42 -0300

    Arquivo da Main

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "funcoes.h"
+  
+/*  LOGINS - CPF - SENHA
+"Gilmario",  123,   12,    3);  gerente
+"Alonso",    125,   25,    2);  transportador
+"Ze",        888,   88,    2);  ...
+"Yuri",      124,   24,    1);  secretario
+"Paula",     126,   26,    1);  ...
+"Felipe",    222,   22,    1);  ...
+"Levi",      555,   55,    1);  ... 
+"Alice",     333,   33,    1);  ...
+*/
+
+int main(){
+    menu();
+    return 0;   
+}
